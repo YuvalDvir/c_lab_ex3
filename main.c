4daa24e47a8096005138d33e39f7ce66d6dde8e0
Yuval Dvir <dvir.yuval@gmail.com>
2022-01-06 10:13:40 +0200

    tester is working, without SVN

--- main.c
+++ main.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
-#include <stdbool.h>
 #include <stdlib.h>
 #include <time.h>
 #include <netinet/in.h>
 #include <string.h>
 #include <zconf.h>
 
+#define HTTP_PORT 80
 #define NUM_OF_SERVERS 3
+#define NUM_OF_CLIENTS 1
 #define MIN_PORT_NUM 1024
 #define MAX_PORT_NUM 64000
-#define MESSAGE_SIZE 100
-#define ONE 1
-#define TWO 2
-#define THREE 3
-#define FOUR 4
+#define MESSAGE_SIZE 128
+#define FOUND_END_OF_LINE(c) (*c == '\r') && (*(c+1) == '\n') && (*(c+2) == '\r') && (*(c+3) == '\n')
+#define POINTER_EXISTS(c) (c+4)
+#define GET_CURR_SERVER(c) ((c + 1) % 3)
 
 #define SERVER_PORT_FILE_NAME "server_port"
 #define CLIENT_PORT_FILE_NAME "http_port"
@@ -31,16 +31,17 @@ typedef enum {
 } socket_e;
 
 int get_random_port() {
-    int r;
-    r = rand();
+    int random_port;
+    random_port = rand();
 
-    while (r < MIN_PORT_NUM || r > MAX_PORT_NUM) {
-        r = rand();
+    if (random_port < MIN_PORT_NUM || random_port > MAX_PORT_NUM) {
+        /* Check if the random port mechanism works appropriately */
+        random_port = HTTP_PORT;
     }
 
-    return r;
-
+    return random_port;
 }
+
 int bind_socket(int *socket_fd, int port) {
     struct sockaddr_in service;
 
@@ -60,28 +61,36 @@ int bind_socket(int *socket_fd, int port) {
 void write_socket_port_to_file(int port, int type) {
     FILE *f;
 
-    if (type == CLIENT) {
-        f = fopen(CLIENT_PORT_FILE_NAME, "w");
-    } else {
-        f = fopen(SERVER_PORT_FILE_NAME, "w");
+    /* Decide case to determine which file to open */
+    switch (type) {
+        case CLIENT:
+            f = fopen(CLIENT_PORT_FILE_NAME, "w");
+            break;
+        case SERVER:
+            f = fopen(SERVER_PORT_FILE_NAME, "w");
+            break;
+        default:
+            break;
     }
 
+    /* Handle failure */
     if (f == NULL) {
         printf("Error opening file!\n");
         exit(1);
     }
 
+    /* Write to file */
     fprintf(f, "%d", port);
-    fclose(f);
 
+    fclose(f);
 }
 
 void listen_to_socket(int const *socket_fd, int type) {
     if (type == CLIENT) {
-        listen(*socket_fd, ONE);
+        listen(*socket_fd, NUM_OF_CLIENTS);
     }
     else if (type == SERVER) {
-        listen(*socket_fd, THREE);
+        listen(*socket_fd, NUM_OF_SERVERS);
     }
 }
 
@@ -89,9 +98,8 @@ void init_socket(int *socket_fd, int type) {
     int port;
     port = get_random_port();
 
-
     /* Bind random port */
-    while (bind_socket(socket_fd, port) != 0) {
+    while (bind_socket(socket_fd, port) < 0) {
         port = get_random_port();
     }
 
@@ -105,7 +113,10 @@ void init_socket(int *socket_fd, int type) {
 void init_sockets(int* client_socket, int* server_socket) {
     srand(time(0));
 
+    /* Client */
     init_socket(client_socket, CLIENT);
+
+    /* Server */
     init_socket(server_socket, SERVER);
 }
 
@@ -119,77 +130,101 @@ int find_number_of_occurrences(char const *string, char *substring) {
         tmp = strstr(tmp, substring);
     }
 
-    // printf("%s: found %d occurrences\n", __func__, count);
     return count;
 }
 
-char *get_message(int socket, int type) {
-    char *message = malloc(MESSAGE_SIZE);
+void receive_message(char *message, int socket, int type) {
     int num_of_messages = 0, curr_size = 0;
 
+    /*
+     * Iterate as long as number of messages found is different from the given type arg:
+     * 1 messages = client
+     * 2 messages = server
+     * */
     while (num_of_messages != type) {
+        /* Receive message and current message size */
         curr_size += recv(socket, &(message[curr_size]), MESSAGE_SIZE, 0);
+
+        /* Update number of different messages found in the buffer by searching 'end token' */
         num_of_messages = find_number_of_occurrences(message, "\r\n\r\n");
+
+        /* Update message buffer size */
         message = realloc(message,MESSAGE_SIZE + curr_size);
     }
-
-    // printf("%s: size of message is %d, type is %d\n",__func__,curr_size,type);
-    return message;
 }
 
 int get_message_size(char *message, int type) {
     int occurrences = 0, size = 0;
     char *temp = message;
 
-    while (temp+FOUR && occurrences < type) {
-        if ((*temp == '\r') && (*(temp+ONE) == '\n') && (*(temp+TWO) == '\r') && (*(temp+THREE) == '\n')) {
+    while (POINTER_EXISTS(temp) && occurrences < type) {
+        if (FOUND_END_OF_LINE(temp)) {
             occurrences++;
         }
         temp++;
         size++;
     }
 
-    return size+THREE;
+    return size+NUM_OF_SERVERS;
 }
 
-void send_message(int receiver, int sender, int type) {
+void handle_message(int receiver, int sender, int type) {
+    char *message = malloc(MESSAGE_SIZE);
     int message_size;
-    char *message;
 
-    message = get_message(sender, type);
-    //printf("_%d_message: %s\n",type,message);
+    /* Receive */
+    receive_message(message, sender, type);
+
+    /* Send */
     message_size = get_message_size(message, type);
-
     send(receiver, message, message_size, 0);
+
     free(message);
 }
 
+void initiator(int *client_socket, int *server_socket, int *servers) {
+    int i;
+
+    /* Get socket number, bind, write to file and listen */
+    init_sockets(client_socket, server_socket);
+
+    /* Accept servers connections */
+    for (i = 0; i < NUM_OF_SERVERS; i++) {
+        servers[i] = accept(*server_socket, NULL, NULL);
+    }
+}
+
+void executer(int client, int current_server) {
+    /* Client ---> LB ---> Server */
+    handle_message(current_server, client, CLIENT);
+
+    /* Server ---> LB ---> Client */
+    handle_message(client, current_server, SERVER);
+}
+
 void load_balancer() {
-    int client, client_socket, server_socket, i, iterations = 0, curr_server;
+    int client, client_socket, server_socket, curr_server = -1;
     int servers[NUM_OF_SERVERS];
     int stopped = 0;
 
-    /* Get socket number, bind, write to file and listen */
-    init_sockets(&client_socket, &server_socket);
-
-    for (i = 0; i < NUM_OF_SERVERS; i++) {
-        servers[i] = accept(server_socket, NULL, NULL);
-    }
+    /* Init connections */
+    initiator(&client_socket, &server_socket, servers);
 
     while (!stopped) {
-        curr_server = servers[iterations % NUM_OF_SERVERS];
-
-        /* Accept connection from client */
+        /* Accept current connection from client */
         client = accept(client_socket, NULL, NULL);
 
-        /* Client ---> LB ---> Server */
-        send_message(curr_server, client, CLIENT);
+        /* Calculate this way in order to avoid integer overflow */
+        curr_server = GET_CURR_SERVER(curr_server);
 
-        /* Server ---> LB ---> Client */
-        send_message(client, curr_server, SERVER);
+        /*
+         * Execute LB iteration:
+         * Client ---> LB ---> Server ---> LB ---> Client
+         */
+        executer(client, servers[curr_server]);
 
+        /* Close current client connection */
         close(client);
-        iterations++;
     }
 }
 
